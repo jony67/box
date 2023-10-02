ee3b6d1a8aadf2fffb0cbe455388ffd3ab655bec
jony67 <vasyapupckin@rambler.ru>
2023-10-02 22:16:26 +0200

    Стартовое приложение FastAPI

---
+++ backend/backend/app/api.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI(
+    title="Камера хранения",
+    description="Бэкенд, реализующий модель камеры хранения"
+    )
+
+origins = [
+    "http://127.0.0.1:3000",
+    "http://localhost:3000"
+]
+
+
+app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"]
+)
+
+class Status(BaseModel):
+    code: int = 200  
+    status: str = "Ok"
+
+      
+
+@app.get("/")
+async def get_state() -> dict:
+    return dict(Status())
+
+  
+
