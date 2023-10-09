9d80e29e4fb71a736ab4bd0211702ce2ac0714ec
jony67 <vasyapupckin@rambler.ru>
2023-10-09 12:47:24 +0200

    Добалены маршруты в box_router.py

--- backend/backend/app/box_router.py
+++ backend/backend/app/box_router.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
+from backend.app.MyBox import MyBox
 #from backend.settings import settings
 # Отладка
 from backend.settings_dev import settings
@@ -7,12 +8,31 @@ from backend.settings_dev import settings
 
 router = APIRouter()
 
-class Status(BaseModel):
-    """
-        Класс для проверки URL
-    """
-    status: str = "Ok"
+# Экземпляр класса камеры хранения:
+my_box = MyBox()
+#my_box.set_state('Дверь открыта')
 
-@router.get(settings.main_url)
+
+@router.get(settings.status)
 async def get_state() -> dict:
-    return dict(Status())
+    return {"Status": "OK"}
+
+@router.get(settings.state)
+async def get_state() -> dict:
+    state = my_box.state
+    return {"stateBox": state}
+
+@router.get(settings.close)
+async def close_box() -> dict:
+    state = my_box.state
+    if (state != ('Дверь открыта' or 'Закройте дверь')):        
+        return {"stateBox": state}
+    else:
+        my_box.closed()
+        state = my_box.state
+        return {"stateBox": state}
+    
+    
+@router.post(settings.open)
+async def open_box(name: str) -> dict:
+    return {"stateBox": "Good"}   
