e0ed8e4a14d45d1f23694bae287c35fd3d9a9a64
jony67 <vasyapupckin@rambler.ru>
2023-10-13 23:57:31 +0200

    Добавлены тесты

--- backend/tests/test_status.py
+++ backend/tests/test_status.py
@@ -2,12 +2,22 @@ from fastapi.testclient import TestClient
 from backend.app.api import app
 #from backend.settings import settings
 # Отладка
-from backend.settings_dev import settings
+from backend.settings import settings_Dev
+from backend.app.MyBox import MyBox
 
-def test_answer():
+def test_main():
     client = TestClient(app)
-    result = client.get(settings.main_url)
+    result = client.get('/')
     assert result.status_code == 200
     assert result.json() == {
         "Status": "OK"
+    }
+
+def test_state():
+    state = MyBox().state
+    client = TestClient(app)
+    result = client.get('/state')
+    assert result.status_code == 200
+    assert result.json() == {
+        "stateBox": state
     }
